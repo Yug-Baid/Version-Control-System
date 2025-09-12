d7f428517c84981ca4c68761705aa631f13bd440
Yug Baid <yugbaid4@gmail.com>
2025-09-12 22:37:21 +0530

    Added logic in init.js and add.js

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,5 +1,20 @@
-const addRepo = async () => {
-    console.log("Add command is working Fine")
+const fs = require('fs').promises
+const path = require('path')
+
+const addRepo = async (filePath) => {
+    const repoPath = path.resolve(process.cwd(),".Git")
+    const stagingPath = path.join(repoPath,"Staged_Files")
+
+    try {
+        await fs.mkdir(stagingPath,{recursive:true})
+        const fileName = path.basename(filePath)
+        await fs.copyFile(fileName,path.join(stagingPath,fileName))
+        console.log("Done")
+
+        
+    } catch (error) {
+        console.error()
+    }
 }
 
 module.exports = {addRepo}
--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,21 @@
+const fs = require('fs').promises;
+const path = require('path')
+
+
 const initRepo = async () => {
-    console.log("Init command is working Fine")
+   
+    const repoPath = path.resolve(process.cwd(),".Git")
+    const commitPath = path.join(repoPath,"commits")
+    try {
+        await fs.mkdir(repoPath,{recursive:true})
+        await fs.mkdir(commitPath,{recursive:true})
+        await fs.writeFile(
+            path.join(repoPath,"config.json"),
+            JSON.stringify({bucket:"Some thing Useful"})
+        )
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 module.exports = {initRepo}
