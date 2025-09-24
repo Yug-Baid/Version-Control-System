d31eefde427240c83073025419ffb58e7cb81e7f
Yug Baid <yugbaid4@gmail.com>
2025-09-24 11:39:47 +0530

    Completed most of the backend Work and updated Issue Controller

--- backend/controllers/RepoController.js
+++ backend/controllers/RepoController.js
@@ -68,21 +68,84 @@ const fetchRepoByName = async (req,res)=>{
      }
 }
 
-const fetchRepoForCurrentUser = (req,res)=>{
-    res.send('Repo of current User')
+const fetchRepoForCurrentUser = async (req,res)=>{
+    const userId = req.user
+
+    try {
+        const repos = await Repo.findById(id)
+
+        if(!repos || repos.length == 0){
+            return res.status(404).json({message:"Repo Not Found"})
+        }
+
+        res.json({message:"Repos Found",repos})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message:"Server Error"})
+     }
 }
 
-const updateRepoById = (req,res)=>{
-    res.send('Repo Updated')
+const updateRepoById = async(req,res)=>{
+    const {id} = req.params
+    const {content,description} = req.body
+
+    try {
+        const repo = await Repo.findById(id)
+         if(!repos || repos.length == 0){
+            return res.status(404).json({message:"Repo Not Found"})
+        }
+
+        repo.content.push = content
+        repo.description = description
+        const updatedRepo = await repo.save()
+        
+        res.json({message:"Repo Updated",reposiotries:updatedRepo})
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message:"Server Error"})
+     }
 }
 
-const deleteRepoById = (req,res)=>{
-    res.send('Repo Deleted')
+const deleteRepoById = async (req,res)=>{
+    const {id} = req.params;
+
+    try {
+        const repo = await Repo.findByIdAndDelete(id)
+
+        if(!repos || repos.length == 0){
+            return res.status(404).json({message:"Repo Not Found"})
+        }
+
+        res.json({message:"Repo Deleted"})
+
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message:"Server Error"})
+     }
 }
 
-const toggleVisibility = (req,res)=>{
-    res.send('Repo Visibility Changed')
-}
+const toggleVisibility = async (req,res)=>{
+    const {id} = req.params
+   
+    try {
+        const repo = await Repo.findById(id)
+
+         if(!repos || repos.length == 0){
+            return res.status(404).json({message:"Repo Not Found"})
+        }
+
+        repo.visibility = !repo.visibility
+        const updatedRepo = await repo.save()
+        
+        res.json({message:"Repo Visibility Updated",reposiotries:updatedRepo})
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message:"Server Error"})
+     }
+    }
 
 module.exports = {
     createRepo,
