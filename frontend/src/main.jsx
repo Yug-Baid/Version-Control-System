a3b78fc952644603cc3ed426335b6f00dcb6954a
Yug Baid <yugbaid4@gmail.com>
2025-10-14 00:57:47 +0530

    Added Signup and Login Route in Frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Authprovider } from './AuthContext.jsx'
+import ProjectRoutes from './components/Routes.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <Authprovider>
-    <App />
+    <Router>
+    <ProjectRoutes />
+    </Router>
      </Authprovider>
   
 )
